476b69aec1f9f60f64c327acc9c3ee6b543283e0
monjuAflame <monjuaflame@gmail.com>
2023-03-31 22:15:50 +0600

    work with background

--- wp-content/themes/first-dev/functions.php
+++ wp-content/themes/first-dev/functions.php
@@ -6,6 +6,7 @@ add_action('after_setup_theme', function(){
     add_theme_support('custom-header', array(
         'default-image' => get_template_directory_uri() . '/images/a.jpg'
     ));
+    add_theme_support('custom-background');
 });
 
 add_filter('',function(){
--- wp-content/themes/first-dev/index.php
+++ wp-content/themes/first-dev/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name'); ?></title>
 </head>
-<body>
+<body <?php body_class() ?> >
     <h1>Hello World</h1>
 
     <img src="<?php header_image() ?>" alt="">
