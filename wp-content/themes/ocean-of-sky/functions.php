97d863679ff756cd08c5d47e712fe5db581c5736
monjuAflame <monjuaflame@gmail.com>
2023-04-11 11:38:43 +0600

    for main style.css called

--- wp-content/themes/ocean-of-sky/functions.php
+++ wp-content/themes/ocean-of-sky/functions.php
@@ -10,8 +10,6 @@ function basic_theme_function(){
 add_action('wp_enqueue_scripts','basic_theme_styles');
 
 function basic_theme_styles() {
-    wp_enqueue_style('style', get_template_directory_uri() . 'style.css','','');
-    or
     wp_enqueue_style('style', get_stylesheet_uri(),'','');
 }
 
