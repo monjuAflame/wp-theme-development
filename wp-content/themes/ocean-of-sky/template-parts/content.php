2a1f6c095606e22519157b2baf976359e72f04a0
monjuAflame <monjuaflame@gmail.com>
2023-04-11 23:47:37 +0600

    post text trim

--- wp-content/themes/ocean-of-sky/template-parts/content.php
+++ wp-content/themes/ocean-of-sky/template-parts/content.php
@@ -7,7 +7,14 @@
         <a href="<?php the_permalink() ?>"><?php the_title()?></a>
       </div>
       <div class="contentText">
-        <?php the_content() ?>
+        <?php 
+        if(is_single()) :
+            the_content();
+        else :
+            $read_more =  '<p><a href="'.get_permalink().'">...Read more</a></p>';
+            echo wp_trim_words(get_the_content(), '3', $read_more); 
+        endif
+        ?>
       </div>   
 
     <?php endwhile; ?>
